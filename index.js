30e9c20e1a8c37a26fe1285536073b02fe9c68cb
kolach <chikolad@gmail.com>
2016-02-16 23:40:12 -0600

    eslint rules

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var CloudinaryConnector = require('./lib/cloudinary-connector');
+
 CloudinaryConnector.CloudinaryStorageService = require('./lib/cloudinary-storage-service');
 
 module.exports = CloudinaryConnector;
